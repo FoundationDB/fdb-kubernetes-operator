a578f42fb3beb405f3be2e6265c9266c33d1cda6
David Young <davidy@funkypenguin.co.nz>
2020-12-11 09:22:57 +1300

    Revert UID and refine kube-score tests

--- charts/.ci/scripts/local-kube-score.sh
+++ charts/.ci/scripts/local-kube-score.sh
@@ -3,5 +3,8 @@
 helm template --values charts/.ci/values-kube-score.yaml charts/* -f charts/.ci/values-kube-score.yaml | kube-score score - \
     --ignore-test pod-networkpolicy \
     --ignore-test deployment-has-poddisruptionbudget \
-    --ignore-test deployment-has-host-podantiaffinity
+    --ignore-test deployment-has-host-podantiaffinity \
+    --enable-optional-test container-security-context-privileged \
+    --enable-optional-test container-security-context-readonlyrootfilesystem \
+    --ignore-test container-security-context    
 
