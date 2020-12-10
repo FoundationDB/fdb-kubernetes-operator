c7d006e54e1929f29f07c0de053780a5c5ef35af
David Young <davidy@funkypenguin.co.nz>
2020-12-10 10:29:01 +0000

    Address PR feedback

--- charts/.ci/scripts/local-ct-lint.sh
+++ charts/.ci/scripts/local-ct-lint.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-<<<<<<< HEAD
 docker run --rm -it -w /repo -v $(pwd):/repo quay.io/helmpack/chart-testing ct lint --all
-=======
-docker run --rm -it -w /repo -v `pwd`:/repo quay.io/helmpack/chart-testing ct lint --all
->>>>>>> 3b9f0ff (Update chart and add publishing action)
--- charts/.ci/scripts/local-kube-score.sh
+++ charts/.ci/scripts/local-kube-score.sh
@@ -3,12 +3,8 @@
 helm template --values charts/.ci/values-kube-score.yaml charts/* -f charts/.ci/values-kube-score.yaml | kube-score score - \
     --ignore-test pod-networkpolicy \
     --ignore-test deployment-has-poddisruptionbudget \
-<<<<<<< HEAD
     --ignore-test deployment-has-host-podantiaffinity \
     --enable-optional-test container-security-context-privileged \
     --enable-optional-test container-security-context-readonlyrootfilesystem \
     --ignore-test container-security-context    
-
-=======
     --ignore-test deployment-has-host-podantiaffinity
->>>>>>> 3b9f0ff (Update chart and add publishing action)
