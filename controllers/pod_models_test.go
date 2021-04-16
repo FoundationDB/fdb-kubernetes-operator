cfb66a9c734558f58bf1b68edd382fa8b57e890b
Johannes M. Scheuermann <joh.scheuer@gmail.com>
2021-04-16 17:29:22 +0100

    Correct execution order in GH actions and refactored controller tests

--- controllers/pod_models_test.go
+++ controllers/pod_models_test.go
@@ -24,7 +24,6 @@ import (
 	"fmt"
 	"reflect"
 	"strings"
-	"testing"
 
 	fdbtypes "github.com/FoundationDB/fdb-kubernetes-operator/api/v1beta1"
 	. "github.com/onsi/ginkgo"
@@ -3223,55 +3222,47 @@ var _ = Describe("pod_models", func() {
 				}, "test/curImage:6.2.20"),
 		)
 	})
-})
 
-func TestValidateCustomParameters(t *testing.T) {
-	tt := []struct {
-		Name               string
-		Input              []string
-		ExpectedViolations []string
-	}{
-		{
-			Name:               "Valid parameter without duplicate",
-			Input:              []string{"blahblah=1"},
-			ExpectedViolations: []string{},
-		},
-		{
-			Name:               "Valid parameter with duplicate",
-			Input:              []string{"blahblah=1", "blahblah=1"},
-			ExpectedViolations: []string{"found duplicated customParameter: blahblah"},
-		},
-		{
-			Name:               "Protected parameter without duplicate",
-			Input:              []string{"datadir=1"},
-			ExpectedViolations: []string{"found protected customParameter: datadir, please remove this parameter from the customParameters list"},
-		},
-		{
-			Name:               "Valid parameter with duplicate and protected parameter",
-			Input:              []string{"blahblah=1", "blahblah=1", "datadir=1"},
-			ExpectedViolations: []string{"found duplicated customParameter: blahblah", "found protected customParameter: datadir, please remove this parameter from the customParameters list"},
-		},
-	}
+	When("Providing a custom parameter", func() {
+		type testCase struct {
+			Input              []string
+			ExpectedViolations []string
+		}
 
-	for _, tc := range tt {
-		t.Run(tc.Name, func(t *testing.T) {
-			err := ValidateCustomParameters(tc.Input)
-			errMsg := fmt.Errorf("found the following customParameters violations:\n%s", strings.Join(tc.ExpectedViolations, "\n"))
-
-			if err == nil {
-				// No errors expected and no error occurred
-				if len(tc.ExpectedViolations) == 0 {
-					return
+		DescribeTable("should set the expected parameter or print the violation",
+			func(tc testCase) {
+				err := ValidateCustomParameters(tc.Input)
+				var errMsg error
+				if len(tc.ExpectedViolations) > 0 {
+					errMsg = fmt.Errorf("found the following customParameters violations:\n%s", strings.Join(tc.ExpectedViolations, "\n"))
 				}
 
-				t.Logf("expected error:\n%v, but got:\n%v", errMsg, err)
-				t.FailNow()
-			}
-
-			if errMsg.Error() != err.Error() {
-				t.Logf("expected error:\n%v, but got:\n%v", errMsg, err)
-				t.Fail()
-			}
-		})
-	}
-}
+				if err == nil {
+					Expect(len(tc.ExpectedViolations)).To(BeNumerically("==", 0))
+				} else {
+					Expect(err).To(Equal(errMsg))
+				}
+			},
+			Entry("Valid parameter without duplicate",
+				testCase{
+					Input:              []string{"blahblah=1"},
+					ExpectedViolations: []string{},
+				}),
+			Entry("Valid parameter with duplicate",
+				testCase{
+					Input:              []string{"blahblah=1", "blahblah=1"},
+					ExpectedViolations: []string{"found duplicated customParameter: blahblah"},
+				}),
+			Entry("Protected parameter without duplicate",
+				testCase{
+					Input:              []string{"datadir=1"},
+					ExpectedViolations: []string{"found protected customParameter: datadir, please remove this parameter from the customParameters list"},
+				}),
+			Entry("Valid parameter with duplicate and protected parameter",
+				testCase{
+					Input:              []string{"blahblah=1", "blahblah=1", "datadir=1"},
+					ExpectedViolations: []string{"found duplicated customParameter: blahblah", "found protected customParameter: datadir, please remove this parameter from the customParameters list"},
+				}),
+		)
+	})
+})
