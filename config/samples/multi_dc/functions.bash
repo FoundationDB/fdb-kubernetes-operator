2daba92bcae87a6b6fc9025f4e98cbd7a5424a03
Johannes M. Scheuermann <joh.scheuer@gmail.com>
2021-05-25 20:47:11 +0100

    Fix sample setup

--- config/samples/multi_dc/functions.bash
+++ config/samples/multi_dc/functions.bash
@@ -2,20 +2,20 @@ function applyFile() {
 	path=$1
 	dc=$2
 	connectionString=$3
-	if [[ $dataCenter == "dc3" ]]; then
+	if [[ "${dc}" == "dc3" ]]; then
 		logCount=0
 	else
 		logCount=-1
 	fi
 	
-	cat $path | sed -e "s/\$dc/$dc/" | sed -e "s/\$logCount/$logCount/" | sed -e "s/\$connectionString/$connectionString/" | kubectl apply -f -
+  sed -e "s/\$dc/$dc/" "${path}" | sed -e "s/\$logCount/$logCount/" | sed -e "s/\$connectionString/$connectionString/" | kubectl apply -f -
 }
 
 function checkReconciliation() {
 	clusterName=$1
 
-	generationsOutput=$(kubectl get fdb $clusterName -o jsonpath='{.metadata.generation} {.status.generations.reconciled}')
-	read -ra generations <<< $generationsOutput
+	generationsOutput=$(kubectl get fdb "${clusterName}" -o jsonpath='{.metadata.generation} {.status.generations.reconciled}')
+	read -ra generations <<< "${generationsOutput}"
 	if [[ ("${generations[1]}" != "")  && ("${generations[0]}" == "${generations[1]}") ]]; then
 		return 1
 	else
@@ -24,22 +24,20 @@ function checkReconciliation() {
 	fi
 }
 
-
 function getConnectionString() {
 	clusterName=$1
 
-	kubectl get fdb $clusterName -o jsonpath='{.status.connectionString}'
+	kubectl get fdb "${clusterName}" -o jsonpath='{.status.connectionString}'
 }
-
 
 function checkReconciliationLoop() {
 	reconciled=0
 	name=$1
 	while [ $reconciled -ne 1 ] ; do
-		checkReconciliation $name
+		checkReconciliation "${name}"
 		reconciled=$?
 		if [[ $reconciled -ne 1 ]]; then
-			echo "Waiting for reconcilliation"
+			echo "Waiting for reconciliation"
 			sleep 5
 		fi
 	done
