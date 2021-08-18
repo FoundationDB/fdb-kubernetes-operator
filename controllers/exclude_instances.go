6af71a473ff539328022a8ceb8b5d861fd5b35be
John Brownlee <john_brownlee@apple.com>
2021-08-18 12:45:20 -0700

    Allow exclusions when another process is missing.

--- controllers/exclude_instances.go
+++ controllers/exclude_instances.go
@@ -23,12 +23,18 @@ package controllers
 import (
 	ctx "context"
 	"fmt"
+	"math"
 	"net"
 
 	corev1 "k8s.io/api/core/v1"
 
 	fdbtypes "github.com/FoundationDB/fdb-kubernetes-operator/api/v1beta1"
+	"github.com/FoundationDB/fdb-kubernetes-operator/internal"
 )
+
+// The fraction of processes that must be present in order to start a new
+// exclusion.
+var missingProcessThreshold = 0.8
 
 // ExcludeInstances provides a reconciliation step for excluding instances from
 // the database.
@@ -36,8 +42,6 @@ type ExcludeInstances struct{}
 
 // Reconcile runs the reconciler's work.
 func (e ExcludeInstances) Reconcile(r *FoundationDBClusterReconciler, context ctx.Context, cluster *fdbtypes.FoundationDBCluster) *Requeue {
-	logger := log.WithValues("namespace", cluster.Namespace, "cluster", cluster.Name, "reconciler", "ExcludeInstances")
-
 	adminClient, err := r.getDatabaseClientProvider().GetAdminClient(cluster, r)
 	if err != nil {
 		return &Requeue{Error: err}
@@ -52,6 +56,7 @@ func (e ExcludeInstances) Reconcile(r *FoundationDBClusterReconciler, context ct
 	}
 
 	addresses := make([]fdbtypes.ProcessAddress, 0, removalCount)
+	processClassesToExclude := make(map[fdbtypes.ProcessClass]internal.None)
 	if removalCount > 0 {
 		exclusions, err := adminClient.GetExclusions()
 		if err != nil {
@@ -67,28 +72,18 @@ func (e ExcludeInstances) Reconcile(r *FoundationDBClusterReconciler, context ct
 			for _, address := range processGroup.Addresses {
 				if processGroup.Remove && !processGroup.ExclusionSkipped && !currentExclusionMap[address] {
 					addresses = append(addresses, fdbtypes.ProcessAddress{IPAddress: net.ParseIP(address)})
+					processClassesToExclude[processGroup.ProcessClass] = internal.None{}
 				}
 			}
 		}
 	}
 
 	if len(addresses) > 0 {
-		// Block excludes on missing processes not marked for removal
-		missingProcesses := make([]string, 0)
-		for _, processGroupStatus := range cluster.Status.ProcessGroups {
-			if processGroupStatus.Remove {
-				continue
+		for processClass := range processClassesToExclude {
+			canExclude, missingProcesses := canExcludeNewInstances(cluster, processClass)
+			if !canExclude {
+				return &Requeue{Message: fmt.Sprintf("Waiting for missing processes: %v. Addresses to exclude: %v", missingProcesses, addresses)}
 			}
-
-			// TODO(johscheuer): we should add here a threshold for how many missing processes are fine
-			if processGroupStatus.GetConditionTime(fdbtypes.MissingProcesses) != nil ||
-				processGroupStatus.GetConditionTime(fdbtypes.MissingPod) != nil {
-				missingProcesses = append(missingProcesses, processGroupStatus.ProcessGroupID)
-				logger.Info("Missing processes", "processGroupID", processGroupStatus.ProcessGroupID)
-			}
-		}
-		if len(missingProcesses) > 0 {
-			return &Requeue{Message: fmt.Sprintf("Waiting for missing processes: %v", missingProcesses)}
 		}
 
 		hasLock, err := r.takeLock(cluster, fmt.Sprintf("excluding instances: %v", addresses))
@@ -105,4 +100,38 @@ func (e ExcludeInstances) Reconcile(r *FoundationDBClusterReconciler, context ct
 	}
 
 	return nil
+}
+
+func canExcludeNewInstances(cluster *fdbtypes.FoundationDBCluster, processClass fdbtypes.ProcessClass) (bool, []string) {
+	logger := log.WithValues("namespace", cluster.Namespace, "cluster", cluster.Name, "reconciler", "ExcludeInstances")
+
+	// Block excludes on missing processes not marked for removal
+	missingProcesses := make([]string, 0)
+	validProcesses := make([]string, 0)
+
+	for _, processGroupStatus := range cluster.Status.ProcessGroups {
+		if processGroupStatus.Remove || processGroupStatus.ProcessClass != processClass {
+			continue
+		}
+
+		if processGroupStatus.GetConditionTime(fdbtypes.MissingProcesses) != nil ||
+			processGroupStatus.GetConditionTime(fdbtypes.MissingPod) != nil {
+			missingProcesses = append(missingProcesses, processGroupStatus.ProcessGroupID)
+			logger.Info("Missing processes", "processGroupID", processGroupStatus.ProcessGroupID)
+		} else {
+			validProcesses = append(validProcesses, processGroupStatus.ProcessGroupID)
+		}
+	}
+
+	desiredProcesses, err := cluster.GetProcessCountsWithDefaults()
+	if err != nil {
+		logger.Error(err, "Error calculating process counts")
+		return false, missingProcesses
+	}
+	desiredCount := desiredProcesses.Map()[processClass]
+
+	if len(validProcesses) < desiredCount-1 && len(validProcesses) < int(math.Ceil(float64(desiredCount)*missingProcessThreshold)) {
+		return false, missingProcesses
+	}
+	return true, nil
 }
