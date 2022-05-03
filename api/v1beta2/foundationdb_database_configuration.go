a19afc6d3952d17f73b37448b32d370b43a30469
Johannes Scheuermann <johscheuer@users.noreply.github.com>
2022-05-03 07:20:09 +0100

    For versions that support grv and commit use grv and commit proxy count if defined (#1191)

--- api/v1beta2/foundationdb_database_configuration.go
+++ api/v1beta2/foundationdb_database_configuration.go
@@ -307,6 +307,7 @@ func (configuration *DatabaseConfiguration) GetRoleCountsWithDefaults(version Ve
 			counts.LogRouters = -1
 		}
 	}
+
 	return *counts
 }
 
@@ -552,7 +553,7 @@ func (configuration DatabaseConfiguration) getRegionPriorities() map[string]int 
 // to 0
 func (configuration DatabaseConfiguration) AreSeparatedProxiesConfigured() bool {
 	counts := configuration.RoleCounts
-	return counts.Proxies == 0 && (counts.GrvProxies > 0 || counts.CommitProxies > 0)
+	return counts.GrvProxies > 0 || counts.CommitProxies > 0
 }
 
 // GetProxiesString returns a string that contains the correct fdbcli
