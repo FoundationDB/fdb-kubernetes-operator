de48344a417c39c2aa1f584d76c579e8e51e9273
Johannes M. Scheuermann <jscheuermann@apple.com>
2022-11-07 15:20:31 +0000

    Fix lint

--- pkg/fdbadminclient/mock/suite_test.go
+++ pkg/fdbadminclient/mock/suite_test.go
@@ -23,11 +23,29 @@ package mock
 import (
 	"testing"
 
+	fdbv1beta2 "github.com/FoundationDB/fdb-kubernetes-operator/api/v1beta2"
+	mockclient "github.com/FoundationDB/fdb-kubernetes-operator/mock-kubernetes-client/client"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"k8s.io/client-go/kubernetes/scheme"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 )
+
+var k8sClient *mockclient.MockClient
 
 func TestCmd(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Mock Admin Client")
 }
+
+var _ = BeforeSuite(func() {
+	logf.SetLogger(zap.New(zap.UseDevMode(true), zap.WriteTo(GinkgoWriter)))
+	Expect(scheme.AddToScheme(scheme.Scheme)).NotTo(HaveOccurred())
+	Expect(fdbv1beta2.AddToScheme(scheme.Scheme)).NotTo(HaveOccurred())
+	k8sClient = mockclient.NewMockClient(scheme.Scheme)
+})
+
+var _ = AfterEach(func() {
+	k8sClient.Clear()
+})
