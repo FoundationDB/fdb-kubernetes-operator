d67ca270c0a624782c1a5e08d795cbf39c072c01
Meng Xu <meng_xu@apple.com>
2023-01-20 21:46:01 -0800

    Add recommended running env for setup_e2e.sh

--- scripts/setup_e2e.sh
+++ scripts/setup_e2e.sh
@@ -16,6 +16,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# This script works out of box for Linux env (tested on Ubuntu 16.04.5 LTS), after installing dependent software;
+# It's not recommended to run on Mac without Docker native support because it may involve non-trivial debug effort.
 set -eo errexit
 
 # wait_for_node_setup is used to wait until the kubelet has started and the Pod CIDR is available.
