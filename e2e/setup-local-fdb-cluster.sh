ffdef58f08dd8180fe411afb5e10407464e6ab85
Meng Xu <meng_xu@apple.com>
2023-03-08 10:51:52 -0800

    Update PR action: Repace setup_kind_local_registry.sh
    Replace setup_kind_local_registry.sh with e2e/setup-local-fdb-cluster.sh
    
    Add TODO and assumptions to e2e/setup-local-fdb-cluster.sh

--- e2e/setup-local-fdb-cluster.sh
+++ e2e/setup-local-fdb-cluster.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 # Set up local FDB non-HA cluster on a 4-node kind k8s cluster
 # This only works on x86 machines as FDB doesn't provide arm64 Linux binaries yet
+# Assumptions: (1) There is at most one kind cluster in the env; (2) kubectl is pointing to the kind cluster
 read -p "create kind cluster? (enter yes or no): " createKindCluster
 
 cluster=${cluster:-"local-cluster"}
 
 if [ "${createKindCluster}" = "yes" ]; then
     echo "===Start creating k8s cluster on kind"
-    #../scripts/setup_kind_local_registry.sh # the script doesn't work 
     kind create cluster --name ${cluster} --config ./local-cluster-config.yaml
 else
     echo "===Skip creating k8s cluster on kind"
+    # TODO: make sure kind is using the local-cluster context
     kind get clusters
 fi
 
@@ -30,8 +31,9 @@ kubectl apply -k ./config/tests/base
 
 echo "===Done==="
 
+# TODO: Make sure kubectl context is pointing to the kind cluster
 kubectl get fdb
-echo "Waiting for creating fdb pods..."
+echo "Waiting for creating FDB Pods..."
 sleep 2;
 kubectl wait --for=condition=ready pod -l foundationdb.org/fdb-cluster-name=test-cluster
 
