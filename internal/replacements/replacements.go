a922c9ffcdbf7f89d69ce61c7a0092f24a83ef9d
Johannes Scheuermann <johscheuer@users.noreply.github.com>
2023-06-15 06:53:29 +0200

    Add better logging for spec hash changes (#1670)
    * Add better logging for spec hash changes

--- internal/replacements/replacements.go
+++ internal/replacements/replacements.go
@@ -22,6 +22,8 @@ package replacements
 
 import (
 	"context"
+	"encoding/base64"
+	"encoding/json"
 	"fmt"
 
 	"github.com/FoundationDB/fdb-kubernetes-operator/pkg/podmanager"
@@ -200,14 +202,28 @@ func processGroupNeedsRemoval(cluster *fdbv1beta2.FoundationDBCluster, pod *core
 	}
 
 	if cluster.NeedsReplacement(processGroupStatus) {
-		specHash, err := internal.GetPodSpecHash(cluster, processGroupStatus.ProcessClass, idNum, nil)
+		spec, err := internal.GetPodSpec(cluster, processGroupStatus.ProcessClass, idNum)
+		if err != nil {
+			return false, err
+		}
+
+		specHash, err := internal.GetPodSpecHash(cluster, processGroupStatus.ProcessClass, idNum, spec)
 		if err != nil {
 			return false, err
 		}
 
 		if pod.ObjectMeta.Annotations[fdbv1beta2.LastSpecKey] != specHash {
+			jsonSpec, err := json.Marshal(spec)
+			if err != nil {
+				return false, err
+			}
+
 			logger.Info("Replace process group",
-				"reason", fmt.Sprintf("specHash has changed from %s to %s", specHash, pod.ObjectMeta.Annotations[fdbv1beta2.LastSpecKey]))
+				"reason", "specHash has changed",
+				"desiredSpecHash", specHash,
+				"currentSpecHash", pod.ObjectMeta.Annotations[fdbv1beta2.LastSpecKey],
+				"desiredSpec", base64.StdEncoding.EncodeToString(jsonSpec),
+			)
 			return true, nil
 		}
 	}
