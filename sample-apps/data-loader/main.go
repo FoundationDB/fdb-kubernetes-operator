0b3cff62c74e300fccd4a678a924ec8e6beedcb1
Johannes M. Scheuermann <jscheuermann@apple.com>
2025-03-21 17:44:03 +0100

    Add support in the data loader to write to multiple clusters

--- sample-apps/data-loader/main.go
+++ sample-apps/data-loader/main.go
@@ -7,7 +7,11 @@ import (
 	"github.com/google/uuid"
 	"log"
 	"math/rand/v2"
+	"os"
 	"os/signal"
+	"path"
+	"strings"
+	"sync"
 	"syscall"
 	"time"
 
@@ -36,10 +40,11 @@ func initRandomGenerator() *rand.ChaCha8 {
 	return rand.NewChaCha8(seed)
 }
 
-func loadData(ctx context.Context, keys int, batchSize int, valueSize int) {
+func loadData(ctx context.Context, keys int, batchSize int, valueSize int, readValues bool, clusterFile string) {
 	batchCount := keys / batchSize
 
-	db, err := fdb.OpenDefault()
+	log.Println("opening database with cluster file:", clusterFile)
+	db, err := fdb.OpenDatabase(clusterFile)
 	if err != nil {
 		log.Fatalf("could not open database: %s", err)
 	}
@@ -53,9 +58,9 @@ func loadData(ctx context.Context, keys int, batchSize int, valueSize int) {
 			return
 		default:
 			log.Println("Writing batch", i)
-			_, err = db.Transact(func(transaction fdb.Transaction) (interface{}, error) {
-				prefix := uuid.NewString()
 
+			prefix := uuid.NewString()
+			_, err = db.Transact(func(transaction fdb.Transaction) (interface{}, error) {
 				for idx := 0; idx < batchSize; idx++ {
 					token := make([]byte, valueSize)
 					_, randErr := randomGen.Read(token)
@@ -72,6 +77,31 @@ func loadData(ctx context.Context, keys int, batchSize int, valueSize int) {
 			if err != nil {
 				log.Printf("could not write data: %s\n", err)
 			}
+
+			if !readValues {
+				continue
+			}
+
+			// Wait one second to give the FDB cluster some time to pull the mutations from the log processes.
+			time.Sleep(1 * time.Second)
+
+			values, err := db.ReadTransact(func(transaction fdb.ReadTransaction) (interface{}, error) {
+				prefixKey, err := fdb.PrefixRange(tuple.Tuple{prefix}.Pack())
+				if err != nil {
+					return nil, err
+				}
+
+				return transaction.GetRange(prefixKey, fdb.RangeOptions{}).GetSliceWithError()
+			})
+
+			keyValues, ok := values.([]fdb.KeyValue)
+			if ok {
+				log.Println("found", len(keyValues), "key values for prefix", prefix)
+			}
+
+			if err != nil {
+				log.Printf("could not write data: %s\n", err)
+			}
 		}
 	}
 }
@@ -79,13 +109,44 @@ func loadData(ctx context.Context, keys int, batchSize int, valueSize int) {
 func main() {
 	fdb.MustAPIVersion(710)
 
-	keys := flag.Int("keys", 100000, "Number of keys to generate")
-	batchSize := flag.Int("batch-size", 10, "Number of bytes to include in each value")
-	valueSize := flag.Int("value-size", 1000, "Number of bytes to include in each value")
+	var keys, batchSize, valueSize int
+	var readValues bool
+	var clusterFileDirectory string
+	flag.IntVar(&keys, "keys", 100000, "Number of keys to generate")
+	flag.IntVar(&batchSize, "batch-size", 10, "Number of updates per batch")
+	flag.IntVar(&valueSize, "value-size", 1000, "Number of bytes to include in each value")
+	flag.BoolVar(&readValues, "read-values", false, "Read the newly written values from the database")
+	flag.StringVar(&clusterFileDirectory, "cluster-file-directory", "", "The directory that contains the cluster file, if empty the data loader will fallback to the $FDB_CLUSTER_FILE")
 	flag.Parse()
 
 	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 	defer stop()
 
-	loadData(ctx, *keys, *batchSize, *valueSize)
+	var wg sync.WaitGroup
+	if clusterFileDirectory == "" {
+		wg.Add(1)
+		go func() {
+			loadData(ctx, keys, batchSize, valueSize, readValues, os.Getenv("FDB_CLUSTER_FILE"))
+			wg.Done()
+		}()
+	} else {
+		entries, err := os.ReadDir(clusterFileDirectory)
+		log.Println("reading cluster files from", clusterFileDirectory)
+		if err != nil {
+			log.Fatalf("could not read directory %s: %s", clusterFileDirectory, err)
+		}
+		for _, entry := range entries {
+			if !strings.HasSuffix(entry.Name(), ".cluster") {
+				continue
+			}
+
+			wg.Add(1)
+			go func() {
+				loadData(ctx, keys, batchSize, valueSize, readValues, path.Join(clusterFileDirectory, entry.Name()))
+				wg.Done()
+			}()
+		}
+	}
+
+	wg.Wait()
 }
