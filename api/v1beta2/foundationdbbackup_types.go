0f7fa176c6a5e57b60e363152299b919bd594baa
Milla Samuel <millas@palantir.com>
2025-06-25 09:45:09 +0800

    Generate

--- api/v1beta2/foundationdbbackup_types.go
+++ api/v1beta2/foundationdbbackup_types.go
@@ -98,7 +98,7 @@ type FoundationDBBackupSpec struct {
 	BlobStoreConfiguration *BlobStoreConfiguration `json:"blobStoreConfiguration,omitempty"`
 
 	// The path to the encryption key used to encrypt the backup.
-	// +kubebuilder:validation:MaxLength=512
+	// +kubebuilder:validation:MaxLength=4096
 	EncryptionKeyPath string `json:"encryptionKeyPath,omitempty"`
 
 	// MainContainer defines customization for the foundationdb container.
