dfae7e353f191c73a0ff5bf631658327b55d1f59
harsh maheshwari <09harshmaheshwari@gmail.com>
2025-12-17 16:36:46 +0530

    fmt changes

--- e2e/test_operator_backups/operator_backup_test.go
+++ e2e/test_operator_backups/operator_backup_test.go
@@ -185,7 +185,7 @@ var _ = Describe("Operator Backup", Label("e2e", "pr"), func() {
 
 							restorable := describeData["Restorable"].(bool)
 							Expect(restorable).To(BeTrue())
-							
+
 							// TODO (09harsh): Uncomment this when we have the fileLevelEncryption in json parser
 							// here: https://github.com/apple/foundationdb/blob/main/fdbclient/BackupContainer.actor.cpp#L193-L250
 							//fileLevelEncryption := describeData["FileLevelEncryption"].(bool)
