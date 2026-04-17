dc684eaa303fe14f720e0374abadbfe5b55ab4c5
Arni Dagur <arni@x.com>
2026-04-17 03:17:09 +0100

    add tests

--- controllers/cluster_controller_test.go
+++ controllers/cluster_controller_test.go
@@ -2940,7 +2940,7 @@ var _ = Describe("cluster_controller", func() {
 			})
 		})
 
-		Context("with a failing Pod", func() {
+		Context("with an old NodeAffinity failed Pod", func() {
 			var recreatedPod corev1.Pod
 
 			BeforeEach(func() {
@@ -2952,6 +2952,72 @@ var _ = Describe("cluster_controller", func() {
 				recreatedPod = pods.Items[0]
 				Expect(
 					k8sClient.SetPodIntoFailed(context.Background(), &recreatedPod, "NodeAffinity"),
+				).NotTo(HaveOccurred())
+				generationGap = 0
+			})
+
+			It("should recreate the Pod", func() {
+				pod := &corev1.Pod{}
+				Expect(
+					k8sClient.Get(
+						context.TODO(),
+						client.ObjectKey{
+							Namespace: recreatedPod.Namespace,
+							Name:      recreatedPod.Name,
+						},
+						pod,
+					),
+				).NotTo(HaveOccurred())
+				Expect(pod.UID).NotTo(Equal(recreatedPod.UID))
+			})
+		})
+
+		Context("with a recent NodeAffinity failed Pod", func() {
+			var recentPod corev1.Pod
+
+			BeforeEach(func() {
+				pods := &corev1.PodList{}
+				Expect(
+					k8sClient.List(context.TODO(), pods, getListOptions(cluster)...),
+				).NotTo(HaveOccurred())
+
+				recentPod = pods.Items[0]
+				recentPod.Status.Phase = corev1.PodFailed
+				recentPod.Status.Reason = "NodeAffinity"
+				recentPod.CreationTimestamp = metav1.Now()
+				Expect(k8sClient.Status().Update(context.TODO(), &recentPod)).NotTo(HaveOccurred())
+				generationGap = 0
+				shouldCompleteReconciliation = false
+			})
+
+			It("should not recreate the Pod yet (due to 5m delay)", func() {
+				pod := &corev1.Pod{}
+				Expect(
+					k8sClient.Get(
+						context.TODO(),
+						client.ObjectKey{
+							Namespace: recentPod.Namespace,
+							Name:      recentPod.Name,
+						},
+						pod,
+					),
+				).NotTo(HaveOccurred())
+				Expect(pod.UID).To(Equal(recentPod.UID))
+			})
+		})
+
+		Context("with an evicted Pod", func() {
+			var recreatedPod corev1.Pod
+
+			BeforeEach(func() {
+				pods := &corev1.PodList{}
+				Expect(
+					k8sClient.List(context.TODO(), pods, getListOptions(cluster)...),
+				).NotTo(HaveOccurred())
+
+				recreatedPod = pods.Items[0]
+				Expect(
+					k8sClient.SetPodIntoFailed(context.Background(), &recreatedPod, "Evicted"),
 				).NotTo(HaveOccurred())
 				generationGap = 0
 			})
